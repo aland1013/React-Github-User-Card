c737d88a00aedc4d23438d7262d620e7a9979530
aland1013 <adam.land@gmail.com>
2020-05-08 23:02:34 -0400

    mvp complete

--- react-github-usercard/src/App.js
+++ react-github-usercard/src/App.js
@@ -1,56 +1,24 @@
 import React from 'react';
 import axios from 'axios';
 import UserCard from './components/UserCard';
-import { Container, Divider, Header, Card } from 'semantic-ui-react';
-
-
-const userArray = ['aland1013', 'tetondan', 'dustinmyers', 'justsml', 'luishrd', 'bigknell'];
+import { Container, Divider, Header } from 'semantic-ui-react';
 
 class App extends React.Component {
   state = {
-    users: [
-      // { avatar_url: "https://avatars0.githubusercontent.com/u/20908636?v=4",
-      // bio: "Experienced lawyer | aspiring legal engineer / blockchain developer",
-      // blog: "",
-      // company: null,
-      // created_at: "2016-08-08T15:15:58Z",
-      // email: null,
-      // events_url: "https://api.github.com/users/aland1013/events{/privacy}",
-      // followers: 1,
-      // followers_url: "https://api.github.com/users/aland1013/followers",
-      // following: 0,
-      // following_url: "https://api.github.com/users/aland1013/following{/other_user}",
-      // gists_url: "https://api.github.com/users/aland1013/gists{/gist_id}",
-      // gravatar_id: "",
-      // hireable: null,
-      // html_url: "https://github.com/aland1013",
-      // id: 20908636,
-      // location: "Athens, GA",
-      // login: "aland1013",
-      // name: "Adam Land",
-      // node_id: "MDQ6VXNlcjIwOTA4NjM2",
-      // organizations_url: "https://api.github.com/users/aland1013/orgs",
-      // public_gists: 1,
-      // public_repos: 48,
-      // received_events_url: "https://api.github.com/users/aland1013/received_events",
-      // repos_url: "https://api.github.com/users/aland1013/repos",
-      // site_admin: false,
-      // starred_url: "https://api.github.com/users/aland1013/starred{/owner}{/repo}",
-      // subscriptions_url: "https://api.github.com/users/aland1013/subscriptions",
-      // type: "User",
-      // updated_at: "2020-05-08T17:04:03Z",
-      // url: "https://api.github.com/users/aland1013"}
-    ]
+    user: null,
+    followers: []
   };
 
   componentDidMount() {
-    userArray.forEach(user => {
-      axios.get(`https://api.github.com/users/${user}`)
+    axios.get('https://api.github.com/users/aland1013')
       .then(response => {
-        console.log(response.data);
-        this.setState({ users: [...this.state.users, response.data] });
+        this.setState({ user: response.data });
       });
-    });
+    
+    axios.get('https://api.github.com/users/aland1013/followers')
+      .then(response => {
+        this.setState({ followers: response.data });
+      });
   }
 
   render() {
@@ -59,11 +27,11 @@ class App extends React.Component {
         <Divider hidden />
         <Header as='h1' textAlign='center'>React Github Usercard Project</Header>
         <Divider hidden />
-        <Card.Group itemsPerRow={2}>
-        {this.state.users.map(user => {
-          return <UserCard key={user.id} user={user} />;
-        })}
-        </Card.Group>
+        {this.state.user === null || this.state.followers.length === 0 ? (
+          <h2>Loading...</h2>
+        ) : (
+          <UserCard user={this.state.user} followers={this.state.followers}/>
+        )}
       </Container>
     );
   }
--- react-github-usercard/src/components/UserCard.js
+++ react-github-usercard/src/components/UserCard.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Card, Image, Icon } from 'semantic-ui-react';
+import { Card, Image, Header, Icon, List } from 'semantic-ui-react';
 
 const UserCard = (props) => {
   const { 
@@ -8,31 +8,42 @@ const UserCard = (props) => {
           login,
           location,
           html_url,
-          followers,
-          following,
           bio
         } = props.user;
+  
+  const { followers } = props;
+  
+  console.log(avatar_url);
 
   return (
     <>
-      <Card centered raised>
+      <Card href={html_url} target="_blank" centered raised>
         <Image src={avatar_url} alt="avatar" />
         <Card.Content>
           <Card.Header>{name}</Card.Header>
           <Card.Meta>{login}</Card.Meta>
           <Card.Description>
             Location: {location}<br/>
-            Profile: 
-            <a 
-              href={html_url}
-              target="_blank"
-            >{html_url}</a><br/>
             Bio: {bio}
           </Card.Description>
         </Card.Content>
-        <Card.Content extra>
-          <Icon color="red" name="users" /> {followers} followers<br/>
-          <Icon color="blue" name="users" /> {following} following<br/>
+        <Card.Content>
+          <Header as="h4" icon>
+            <Icon name="users" circular />
+            Followers
+          </Header>
+          <List size="large">
+            {followers.map(follower => {
+              return (
+                <List.Item as="a" key={follower.id} href={follower.html_url} target="_blank">
+                  <Icon name="user" />
+                  <List.Content>
+                    {follower.login}
+                  </List.Content>
+                </List.Item>
+              );
+            })}
+          </List>
         </Card.Content>
       </Card>
     </>
